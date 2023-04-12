cbfd2f35ffa027c329059d90d4179a6a1d2572f7
Nikumani Choudhury <nikunchoudhury@gmail.com>
2023-04-13 00:59:09 +0530

    Default export and Named Export Prop and PropType (#2)

---
+++ first.mjs
@@ -0,0 +1,8 @@
+const a = "Apple";
+const b = "Mango";
+const c = "Banana";
+const d = "Watermelon";
+
+export default b; //This is default export
+export { a, c }; //This is Named export
+export { d };
---
+++ second.mjs
@@ -0,0 +1,6 @@
+import sec from './first.mjs'
+import { a, c, d } from './first.mjs'
+console.log(sec);
+console.log(a);
+console.log(c);
+console.log(d);
