f1e5b8c4208ec81c6f2858933be2bb0e349026d5
Nikumani Choudhury <nikunchoudhury@gmail.com>
2023-04-15 00:30:27 +0530

    React Route changes (#4)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import DemoPropTypes from "./components/propsType/DemoPropTypes";
+import DemoPropTypes from "./pages/propsType/DemoPropTypes";
 import Navbar from "./components/Navbar.js";
-import Profile from "./components/propsType/Profile.js";
-import Person from "./components/propsType/Person";
+import Profile from "./pages/propsType/Profile.js";
+import Person from "./pages/propsType/Person";
 import {
   Routes,
   Route,
 } from "react-router-dom";
-import About from "./components/About";
+import About from "./pages/About";
 import Footer from "./components/Footer";
-import Home from "./components/Home";
+import Home from "./pages/Home";
+import ReactPropsTypes from "./pages/propsType/ReactPropsTypes";
+import PropsLayout from "./pages/propsType/PropsLayout";
 
 function App() {
   return (
@@ -29,38 +31,55 @@ function App() {
             element={<About />}
           />
 
-          <Route
-            path="/profile"
-            element={
-              <Profile
-                name="Nikumani Choudhury"
-                age={25}
-                gender="Male"
-                address="Sorbhog, Barpeta, Assam"
-                mobile={[1234567890, 9876543210, 78946601348]}
-                render={<DemoPropTypes />}
-              />
-            }
-          />
+          <Route path="/props" element={<PropsLayout />}>
+            <Route index element={<ReactPropsTypes />} />
+            <Route path="demopropstype" element={<DemoPropTypes />} />
+            <Route
+              path="profile"
+              element={
+                <Profile
+                  name="Nikumani Choudhury"
+                  age={25}
+                  gender="Male"
+                  address="Sorbhog, Barpeta, Assam"
+                  mobile={[1234567890, 9876543210, 78946601348]}
+                  render={<DemoPropTypes />}
+                />
+              }
+            />
+
+            <Route
+              path="person"
+              element={
+                <Person   //send props within an object
+                  data={
+                    {
+                      name: "Nik",
+                      age: 25,
+                      gender: "Male",
+                      address: "Sorbhog, Barpeta, Assam"
+                    }
+                  }
+                />
+              }
+            />
+          </Route>
+
+
 
           <Route
-            path="/person"
+            path="*"
             element={
-              <Person   //send props within an object
-                data={
-                  {
-                    name: "Nik",
-                    age: 25,
-                    gender: "Male",
-                    address: "Sorbhog, Barpeta, Assam"
-                  }
-                }
-              />
+              <div className="container">
+                <h1 style={{ 'color': "red" }} > 404 Error !!!</h1>
+                <hr />
+                <h3> Page Not Found !</h3>
+              </div>
             }
           />
-        </Routes>
+        </Routes >
         <Footer />
-      </div>
+      </div >
     </>
 
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import '../css/Footer.css'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 export default function Footer(props) {
     return (
         <footer id='footer'>
             <div className='container'>
-                <ul class="nav">
+                <ul className="nav">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/profile">Profile</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/person">Person</Link>
+                        <NavLink className="nav-link" to="/props">Props and PropsTypes</NavLink>
                     </li>
                 </ul>
             </div>
