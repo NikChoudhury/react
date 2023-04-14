691dfd05f32b8c2d614383928a94c2cf602b6553
Nikumani Choudhury <nikunchoudhury@gmail.com>
2023-04-14 15:44:22 +0530

    React Props And propsTypes

--- first.mjs
+++
@@ -1,8 +0,0 @@
-const a = "Apple";
-const b = "Mango";
-const c = "Banana";
-const d = "Watermelon";
-
-export default b; //This is default export
-export { a, c }; //This is Named export
-export { d };
--- module2.js
+++
@@ -1,7 +0,0 @@
-const a = "Apple";
-const b = "Mango";
-const c = "Banana";
-const d = "Watermelon";
-
-export default a;   // This is default Export
-export { b, c, d }  //This is Named Export
